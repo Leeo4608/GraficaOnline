0fd4abde75f0369547dd2b85c21f7c259a0e384d
Wandalfvx <55510136+Wandalfvx@users.noreply.github.com>
2020-10-22 10:26:35 -0300

    commit wilian.

--- GraficaOnline/Migrations/20201022131811_initial.Designer.cs
+++ GraficaOnline/Migrations/20201022131811_initial.Designer.cs
@@ -4,14 +4,16 @@ using GraficaOnline.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GraficaOnline.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201022131811_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
